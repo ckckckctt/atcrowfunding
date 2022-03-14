e58935a6ac491aa8df0577889a001cd4a5872e37
root <1760078834@qq.com>
2022-03-14 17:26:42 +0800

    分页功能

---
+++ atcrowdfunding01-admin-parent/atcrowdfunding02-admin-webui/src/test/java/CrowdTest.java
@@ -0,0 +1,39 @@
+import com.aiguigu.crowd.entity.Admin;
+import com.atguigu.crowd.mapper.AdminMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring-persist-mybatis.xml")
+public class CrowdTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private AdminMapper adminMapper;
+
+    @Test
+    public void testDataSource() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+    }
+
+
+    @Test
+    public void testMapper(){
+        Admin admin = new Admin(null,"tom","123123","汤姆","tom@qq.com",null);
+        int count = adminMapper.insert(admin);
+        System.out.println("受影响的行数："+count);
+    }
+
+
+}
